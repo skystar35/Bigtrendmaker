fe0143eb8860954d15cad6e0edc5ec82ae2aa4e0
skystar35 <58947650+skystar35@users.noreply.github.com>
2025-11-11 13:47:32 +0100

    Update cors import to @fastify/cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Fastify from 'fastify';
-import cors from 'fastify-cors';
+import cors from '@fastify/cors';
 import { Registry, collectDefaultMetrics, Histogram } from 'prom-client';
 import { createClient } from 'redis';
 import { Queue } from 'bullmq';
